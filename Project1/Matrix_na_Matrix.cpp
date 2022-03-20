7840adc45a0e6332cc791b4cb48c49a9fe89c59c
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-03-20 21:14:39 +0300

    like this but need improvement

--- Project1/Matrix_na_Matrix.cpp
+++ Project1/Matrix_na_Matrix.cpp
@@ -1,50 +1,44 @@
 #include <iostream>
-#include <cstdlib>
 #include <fstream>
-
-
+#include <cstdlib>
 
 using namespace std;
 
-const int n = 5, m = 5;
+const int n = 15, m = 10;
 double A[n][m], B[m][n], v[n], d[n], C[n][n];
 
-
-void FillMatrix(double A1[n][m]) {
-
+void FillMatrix() {
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++) {
-			A1[i][j] = rand()%100;
-
+			A[i][j] = rand() % 100 / 2.5;
 		}
-
 	}
 
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			B[i][j] = rand() % 100 / 2.5;
+		}
+	}
 }
-
 void FillVector(double v1[n]) {
-
-	for (int j = 0; j < m; j++) {
-			v1[j] = rand() % 100;
-
+	for (int j = 0; j < n; j++) {
+		v1[j] = rand() % 100 / 2.5;
 	}
-
 }
 
-void Matrix_Peremnoj(double A1[n][m], double B1[m][n]) {
-
+void Matrix_Peremnoj() {
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+		for (int j = 0; j < n; j++) {
 			C[i][j] = 0;
-			for (int k = 0; k < n; k++) {
-				C[i][j] += A1[i][k] * B1[k][j];
+			for (int k = 0; k < m; k++) {
+				C[i][j] += A[i][k] * B[k][j];
 			}
 		}
 	}
 }
 void Matrix_Peremnoj_na_vector(double A1[n][m], double v1[n]) {
-
 	for (int i = 0; i < n; i++) {
 		d[i] = 0;
 		for (int j = 0; j < m; j++) {
@@ -53,54 +47,46 @@ void Matrix_Peremnoj_na_vector(double A1[n][m], double v1[n]) {
 	}
 }
 
-
 int main() {
-
-	FillMatrix(A);
-	FillMatrix(B);
+	FillMatrix();
 	FillVector(v);
 
 	ofstream File1("Matrix_1.txt");
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++) {
-			File1 << A[i][j]<< " ";
-
+			File1 << A[i][j] << " ";
 		}
 		File1 << endl;
 	}
 	File1.close();
 
 	ofstream File2("Matrix_2.txt");
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < m; i++)
 	{
-		for (int j = 0; j < m; j++) {
+		for (int j = 0; j < n; j++) {
 			File2 << B[i][j] << " ";
-
 		}
 		File2 << endl;
 	}
 	File2.close();
 
-	Matrix_Peremnoj(A, B);
+	Matrix_Peremnoj();
 
 	ofstream File3("Matrix_Otvet.txt");
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < m; j++) {
+		for (int j = 0; j < n; j++) {
 			File3 << C[i][j] << " ";
-
 		}
 		File3 << endl;
 	}
 	File3.close();
 
-
-
 	ofstream File4("Vector_1.txt");
 	for (int i = 0; i < n; i++)
 	{
-		File4 << v[i]<< endl;
+		File4 << v[i] << endl;
 	}
 	File4.close();
 
